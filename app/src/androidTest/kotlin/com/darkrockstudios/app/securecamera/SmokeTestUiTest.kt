564baccbdc9de911c0e79ef1f881e43712b73d99
Adam Brown <adamwbrown@gmail.com>
2025-08-25 12:14:35 -0700

    Smoke test
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test
    
    Smoke test

--- app/src/androidTest/kotlin/com/darkrockstudios/app/securecamera/SmokeTestUiTest.kt
+++ app/src/androidTest/kotlin/com/darkrockstudios/app/securecamera/SmokeTestUiTest.kt
@@ -1,32 +1,20 @@
 package com.darkrockstudios.app.securecamera
 
 import android.Manifest
-import android.app.Application
-import android.content.res.Resources
 import android.os.Build
-import androidx.annotation.StringRes
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.SemanticsProperties
-import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.hasTextExactly
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextClearance
-import androidx.compose.ui.test.performTextInput
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 
 class SmokeTestUiTest {
@@ -49,6 +37,7 @@ class SmokeTestUiTest {
 	@get:Rule
 	val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+	@OptIn(ExperimentalTestApi::class)
 	@Test
 	fun smokeTest() {
 		composeTestRule.apply {
@@ -65,78 +54,32 @@ class SmokeTestUiTest {
 			onNodeWithText(str(R.string.pin_creation_title)).assertIsDisplayed()
 
 			setPinFields("3133734", "313373")
+			waitForIdle()
 			onNodeWithText(str(R.string.pin_creation_button)).performClick()
-			waitForText(R.string.pin_creation_error)
+			waitForIdle()
+			waitUntilAtLeastOneExists(hasTestTag("pin-error"))
 
 			setPinFields("123456", "123456")
 			onNodeWithText(str(R.string.pin_creation_button)).performClick()
-			waitForText(R.string.pin_creation_error_weak_pin)
+			waitForIdle()
+			waitUntilExactlyOneExists(hasText(str(R.string.pin_creation_error_weak_pin)))
 
 			setPinFields("313373", "313373")
 			onNodeWithText(str(R.string.pin_creation_button)).performClick()
+			waitForIdle()
+			waitUntilExactlyOneExists(hasText(str(R.string.pin_creating_vault)))
 
-			waitForTextSimple(str(R.string.pin_creating_vault))
+			waitForEitherTree(hasRole(Role.Button) and hasContentDescription(str(R.string.camera_shutter_button_desc)))
+				.performClick()
 
-			waitForButton(str(R.string.camera_shutter_button_desc))
+			waitForEitherTree(hasContentDescription(str(R.string.camera_more_options_content_description)))
+				.performClick()
 
-			onNode(
-				hasRole(Role.Button) and hasContentDescription(str(R.string.camera_shutter_button_desc))
-			).performClick()
+			waitForEitherTree(hasTestTag("flash-switch"))
+				.performClick()
 
-			waitForContentDescriptionSimple(str(R.string.camera_more_options_content_description))
-
-			onNode(
-				hasRole(Role.Button) and hasContentDescription(str(R.string.camera_more_options_content_description))
-			).performClick()
-
-			waitForTestTagSimple("flash-switch")
-			onNode(
-				hasRole(Role.Switch) and hasTestTag("flash-switch")
-			).performClick()
-
-			onNode(
-				hasRole(Role.Button) and hasContentDescription(str(R.string.camera_close_controls_content_description))
-			).performClick()
-		}
-	}
-
-	private fun waitForButton(contentDescription: String) {
-		composeTestRule.waitUntil(
-			timeoutMillis = 5.seconds.inWholeMilliseconds
-		) {
-			composeTestRule
-				.onAllNodes(hasRole(Role.Button) and hasContentDescription(contentDescription))
-				.fetchSemanticsNodes().isNotEmpty()
-		}
-	}
-
-	private fun waitForTextSimple(text: String) {
-		composeTestRule.waitUntil(
-			timeoutMillis = 5.seconds.inWholeMilliseconds
-		) {
-			composeTestRule
-				.onAllNodes(hasText(text))
-				.fetchSemanticsNodes().isNotEmpty()
-		}
-	}
-
-	private fun waitForContentDescriptionSimple(contentDescription: String) {
-		composeTestRule.waitUntil(
-			timeoutMillis = 5.seconds.inWholeMilliseconds
-		) {
-			composeTestRule
-				.onAllNodes(hasContentDescription(contentDescription))
-				.fetchSemanticsNodes().isNotEmpty()
-		}
-	}
-
-	private fun waitForTestTagSimple(testTag: String) {
-		composeTestRule.waitUntil(
-			timeoutMillis = 5.seconds.inWholeMilliseconds
-		) {
-			composeTestRule
-				.onAllNodes(hasTestTag(testTag))
-				.fetchSemanticsNodes().isNotEmpty()
+			waitForEitherTree(hasRole(Role.Button) and hasContentDescription(str(R.string.camera_close_controls_content_description)))
+				.performClick()
 		}
 	}
 
@@ -150,66 +93,5 @@ class SmokeTestUiTest {
 			placeholder = R.string.pin_creation_confirm_hint,
 			value = confirm,
 		)
-	}
-
-	fun hasRole(role: Role): SemanticsMatcher =
-		SemanticsMatcher.expectValue(SemanticsProperties.Role, role)
-
-	private fun str(@StringRes id: Int): String = r.getString(id)
-	private val r: Resources
-		get() {
-			val application = ApplicationProvider.getApplicationContext<Application>()
-			return application.resources
-		}
-
-	private fun ComposeContentTestRule.waitForText(
-		@StringRes text: Int,
-		timeout: Duration = 10.seconds
-	) {
-		this@waitForText.waitForText(str(text), timeout)
-	}
-
-	fun ComposeContentTestRule.waitForText(
-		text: String,
-		timeout: Duration = 10.seconds,
-		useUnmergedTree: Boolean = true,
-		substring: Boolean = true
-	) {
-		waitUntil(timeout.inWholeMilliseconds) {
-			onAllNodes(
-				hasText(text, substring = substring),
-				useUnmergedTree = useUnmergedTree
-			).fetchSemanticsNodes().isNotEmpty()
-		}
-		onNodeWithText(text, substring = substring)
-			.assertIsDisplayed()
-	}
-
-	fun ComposeContentTestRule.waitForContentDescription(
-		text: String,
-		timeout: Duration = 10.seconds,
-		useUnmergedTree: Boolean = true,
-		substring: Boolean = true
-	) {
-		waitUntil(timeout.inWholeMilliseconds) {
-			onAllNodes(
-				hasText(text, substring = substring),
-				useUnmergedTree = useUnmergedTree
-			).fetchSemanticsNodes().isNotEmpty()
-		}
-		onNodeWithContentDescription(text, substring = substring)
-			.assertIsDisplayed()
-	}
-
-	private fun ComposeContentTestRule.setTextField(value: String, placeholder: Int) {
-		onNode(
-			hasSetTextAction() and hasTextExactly(
-				str(placeholder),
-				includeEditableText = false
-			)
-		).apply {
-			performTextClearance()
-			performTextInput(value)
-		}
 	}
 }
