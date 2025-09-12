8f65d23dabedd7bbd12a6c1c6731fc51a3ef906b
Adam Brown <adamwbrown@gmail.com>
2025-09-11 21:29:11 -0700

    trivial

--- app/src/main/kotlin/com/darkrockstudios/app/securecamera/usecases/VerifyPinUseCase.kt
+++ app/src/main/kotlin/com/darkrockstudios/app/securecamera/usecases/VerifyPinUseCase.kt
@@ -1,12 +1,10 @@
 package com.darkrockstudios.app.securecamera.usecases
 
-import com.darkrockstudios.app.securecamera.auth.AuthorizationRepository
 import com.darkrockstudios.app.securecamera.camera.SecureImageRepository
 import com.darkrockstudios.app.securecamera.security.pin.PinRepository
 import com.darkrockstudios.app.securecamera.security.schemes.EncryptionScheme
 
 class VerifyPinUseCase(
-	private val authManager: AuthorizationRepository,
 	private val imageManager: SecureImageRepository,
 	private val pinRepository: PinRepository,
 	private val encryptionScheme: EncryptionScheme,
@@ -22,7 +20,6 @@ class VerifyPinUseCase(
 		val hashedPin = authorizePinUseCase.authorizePin(pin)
 		return if (hashedPin != null) {
 			encryptionScheme.deriveAndCacheKey(pin, hashedPin)
-			authManager.resetFailedAttempts()
 			true
 		} else {
 			false
